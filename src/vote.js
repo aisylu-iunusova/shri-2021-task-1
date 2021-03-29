2e8f04cb8b4ed7dd06d2e28f86affde486a18c92
Aisylu Iunusova <aisylu.iunusova@gmail.com>
2021-03-29 22:51:31 +0300

    add offset

--- src/vote.js
+++ src/vote.js
@@ -1,13 +1,17 @@
 import { getApp } from "./utils";
 
 class Vote {
-  nextSlide = getApp() === "mobile" ? 9 : 7;
+  nextSlide = getApp() === "mobile" ? 8 : 6;
 
-  template({ users, selectedUserId }) {
+  template({ users, selectedUserId, offset = 0 }) {
+    let prevSlide = offset && offset - this.nextSlide;
+    let nextSlide = offset + this.nextSlide;
+
     return /* html */ `
       <div class="Vote-root">
         <div class="Vote-areas">
             ${users
+              .slice(offset, offset + this.nextSlide)
               .map(({ id, avatar, name }, index) => {
                 const isActive = selectedUserId === id;
 
@@ -15,30 +19,30 @@ class Vote {
                 <div class="Vote-area" data-action="update" data-params='{ \"alias\": \"leaders\", \"data\": { \"selectedUserId\": ${id} }}' style="grid-area: area${
                   index + 1
                 };" data-active="${isActive ? "true" : "false"}">
-                        <div class="User-root" id="${id}">
-                            <div class="User-emoji">
-                                ${isActive ? "👍" : ""}
-                            </div>
-                            <img
-                                class="User-avatar"
-                                src="/assets/images/4x/${avatar}"
-                            />
-                            <div class="User-name">${name}</div>
-                        </div>
+                  <div class="User-root" id="${id}">
+                    <div class="User-emoji">
+                        ${isActive ? "👍" : ""}
                     </div>
+                    <img
+                        class="User-avatar"
+                        src="/assets/images/4x/${avatar}"
+                    />
+                    <div class="User-name">${name}</div>
+                  </div>
+                </div>
                 `;
               })
               .join("")}
 
-            <div class="Vote-area" data-action="update" data-params='{ \"alias\": \"leaders\", \"data\": { \"offset\": ${
-              this.nextSlide
-            } }}' data-nav="up" style="grid-area: nav-button-up;">
-                <button class="Vote-navButton Vote-navButton--prev" disabled></button>
+            <div class="Vote-area" data-action="update" data-params='{ \"alias\": \"leaders\", \"data\": { \"offset\": ${prevSlide} }}' data-nav="up" style="grid-area: nav-button-up;">
+              <button class="Vote-navButton Vote-navButton--prev" disabled="${
+                !offset ? "true" : "false"
+              }"></button>
             </div>
-            <div class="Vote-area" data-action="update" data-params='{ \"alias\": \"leaders\", \"data\": { \"offset\": ${
-              this.nextSlide
-            } }}' data-nav="down" style="grid-area: nav-button-down;">
-                <button class="Vote-navButton Vote-navButton--next"></button>  
+            <div class="Vote-area" data-action="update" data-params='{ \"alias\": \"leaders\", \"data\": { \"offset\": ${nextSlide} }}' data-nav="down" style="grid-area: nav-button-down;">
+              <button class="Vote-navButton Vote-navButton--next" disabled="${
+                users.length <= nextSlide ? "true" : "false"
+              }"></button>  
             </div>
         </div>
       </div>
